b1442d7385f6dc7d146ea8f8bd82475fae2cbac2
germain <germaindev123@gmail.com>
2025-06-17 21:24:39 +0200

    Init maven project

---
+++ src/main/java/org/premsc/analyser/AnalyserApplication.java
@@ -0,0 +1,9 @@
+package org.premsc.analyser;
+
+public class AnalyserApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/org/premsc/analyser/AnalyserApplicationTests.java
@@ -0,0 +1,6 @@
+package org.premsc.analyser;
+
+
+class AnalyserApplicationTests {
+
+}
