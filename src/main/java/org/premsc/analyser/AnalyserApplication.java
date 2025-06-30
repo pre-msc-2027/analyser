78a6ece54cae1f1c4dd62982fe3fc8d449d5e0f2
samiaul <samiaul.56@gmail.com>
2025-06-30 13:39:49 +0200

    feat: Implement API interaction classes and error handling

--- src/main/java/org/premsc/analyser/AnalyserApplication.java
+++ src/main/java/org/premsc/analyser/AnalyserApplication.java
@@ -1,56 +1,59 @@
 package org.premsc.analyser;
 
-import io.github.treesitter.jtreesitter.*;
+import org.premsc.analyser.api.Api;
+import org.premsc.analyser.config.Config;
+import org.premsc.analyser.parser.tree.ITreeHelper;
+import org.premsc.analyser.parser.tree.TreeHelperFactory;
+import org.premsc.analyser.repository.Repository;
+import org.premsc.analyser.rules.IRule;
+import org.premsc.analyser.rules.Ruleset;
+import org.premsc.analyser.rules.Warning;
 
-import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.*;
-import java.lang.foreign.Arena;
-import java.lang.foreign.SymbolLookup;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnalyserApplication {
 
+	private final int id;
+
+	private final Api api = new Api(this);
+
+	private AnalyserApplication(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public Api getApi() {
+		return this.api;
+	}
+
+	}
+
+	}
+
+	private void start() {
+
+		this.init();
+		this.run();
+
+	}
+
+	private void init() {
+	}
+
+	private void run() {
+	}
+
 	public static void main(String[] args) {
-		System.loadLibrary("tree-sitter");
 
-		String library = System.mapLibraryName("libs/tree-sitter-html");
-		SymbolLookup symbols = SymbolLookup.libraryLookup(library, Arena.global());
-		Language html = Language.load(symbols, "tree_sitter_html");
+		int id = Integer.parseInt(args[0]);
 
-		Parser parser = new Parser();
-		parser.setLanguage(html);
+		AnalyserApplication app = new AnalyserApplication(id);
 
+		app.start();
 
-		String source = "<div><p>Hello</p></div>";
-		Tree tree = parser.parse(source).orElseThrow();
-		Node root = tree.getRootNode();
-
-		// Build Swing tree
-		DefaultMutableTreeNode swingRoot = buildTree(root, source);
-
-		// GUI setup
-		JTree jtree = new JTree(swingRoot);
-		JFrame frame = new JFrame("CST Viewer");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 800);
-		frame.add(new JScrollPane(jtree), BorderLayout.CENTER);
-		frame.setVisible(true);
 	}
-
-	private static DefaultMutableTreeNode buildTree(Node node, String source) {
-		String label = node.getType() + " [" +
-				node.getStartByte() + ".." + node.getEndByte() + "]";
-
-		try {
-			String snippet = source.substring(node.getStartByte(), node.getEndByte());
-			if (!snippet.isBlank()) label += " → \"" + snippet.replace("\n", "\\n") + "\"";
-		} catch (Exception ignored) {}
-
-		DefaultMutableTreeNode swingNode = new DefaultMutableTreeNode(label);
-		for (int i = 0; i < node.getChildCount(); i++) {
-			swingNode.add(buildTree(node.getChild(i).orElseThrow(), source));
-		}
-		return swingNode;
-	}
-
 }
