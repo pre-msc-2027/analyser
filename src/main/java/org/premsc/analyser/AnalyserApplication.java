e1bc83a19f070d627aea7ec624479d85ece1e84b
samiaul <samiaul.56@gmail.com>
2025-09-02 09:51:30 +0200

    fix: Simplify JSON parsing in RuleParameter constructor and update Maven compiler configuration

--- src/main/java/org/premsc/analyser/AnalyserApplication.java
+++ src/main/java/org/premsc/analyser/AnalyserApplication.java
@@ -12,6 +12,7 @@ import org.premsc.analyser.repository.Repository;
 import org.premsc.analyser.rules.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
--- src/main/java/org/premsc/analyser/rules/RuleParameter.java
+++ src/main/java/org/premsc/analyser/rules/RuleParameter.java
@@ -1,17 +1,15 @@
 package org.premsc.analyser.rules;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 public record RuleParameter(
         String name,
         String default_value
 ) {
     public RuleParameter(JsonElement data) {
-        JsonObject obj = data.getAsJsonObject();
         this(
-                obj.get("name").getAsString(),
-                obj.get("default_value").getAsString()
+                data.getAsJsonObject().get("name").getAsString(),
+                data.getAsJsonObject().get("default_value").getAsString()
         );
     }
 }
