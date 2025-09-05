b283909bc8701c2034e3e5707e638d6b66ec0107
samiaul <samiaul.56@gmail.com>
2025-09-05 09:56:48 +0200

    feat: Add JsonCreator method to ParameterType enum for dynamic deserialization

--- src/main/java/org/premsc/analyser/rules/ParameterType.java
+++ src/main/java/org/premsc/analyser/rules/ParameterType.java
@@ -1,8 +1,16 @@
 package org.premsc.analyser.rules;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum ParameterType {
     @JsonAlias({"enum", "ENUM"})
     ENUM;
+
+    @JsonCreator
+    public static ParameterType forName(String name) {
+        for(ParameterType c: values())
+            if(c.name().equals(name)) return c;
+        return null;
+    }
 }
